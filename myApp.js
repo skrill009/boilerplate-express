ddf486d4bf28757adb1e2c47247b17a3a2dd562f
skrill009 <gpabonita@gmail.com>
2020-12-22 09:58:28 +0800

    using a __dirname to use files in particular path

--- myApp.js
+++ myApp.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get("/",(req, res) =>{
-  res.send("Hello Express");
+  //res.send("Hello Express");
+  res.send(__dirname + "public/index.html");
 });
+
+
 
 
 
