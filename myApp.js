ffdc91ae46c0470660ebd76858d89b1ef33ab9a2
skrill009 <gpabonita@gmail.com>
2020-12-22 10:22:48 +0800

    edit

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get("/",(req, res) =>{
-  //*send a message
-  //res.send("Hello Express");
-  //*send a file
-  res.sendFile(__dirname + "/views/index.html");
-});
+// app.get("/",(req, res) =>{
+//   //*send a message
+      //res.send("Hello Express");
+//   //*send a file
+//   res.sendFile(__dirname + "/views/index.html");
+// });
 
 app.use("/assets", express.static(__dirname + "/public"));
 
