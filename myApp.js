98f38e29eeb405ace1f6b3889f6810a4a0891d9e
skrill009 <gpabonita@gmail.com>
2020-12-23 09:23:08 +0800

    edit middleware

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // * adding a middleware
 app.use((req,res,next)=>{
-  console.log(req.method + " " + req.path + " " + req.ip);
+  console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 
