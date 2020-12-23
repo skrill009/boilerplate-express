cc849f8315f4169bb2b63f3716afdd9c1cd79fb3
skrill009 <gpabonita@gmail.com>
2020-12-23 08:53:49 +0800

    adding dotenv package and .env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 app.get("/",(req, res) =>{
   //*send a message
@@ -8,11 +9,23 @@ app.get("/",(req, res) =>{
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// app.get("/json", (req,res)=>{
+//   res.json({
+//     "message": "Hello json"
+//   });
+// });
+
 app.get("/json", (req,res)=>{
-  res.json({
-    "message": "Hello json"
-  });
-});
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+    res.json(
+      {"message": "HELLO JSON"}
+    )
+  }else{
+    res.json(
+      {"message":"Hello json"}
+    )
+  }
+})
 
 app.use(express.static(__dirname + "/public"));
 app.use("/assets", express.static(__dirname + "/public"));
