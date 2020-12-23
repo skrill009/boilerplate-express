5760c8873f623f2a409e70895ed3f553982cbdc1
skrill009 <gpabonita@gmail.com>
2020-12-23 09:36:04 +0800

    chaining a middleware

--- myApp.js
+++ myApp.js
@@ -8,12 +8,25 @@ app.use((req,res,next)=>{
   next();
 });
 
+function getCurrentTime(){
+  return new Date().toString();
+}
+
+// * chaining middleware
+app.get('/now', (req,res,next)=>{
+  req.time = getCurrentTime();
+  next();
+}, function(req,res){
+  res.json({"time": req.time});
+});
+
 app.get("/",(req, res) =>{
   //*send a message
   //res.send("Hello Express");
   //*send a file 
   res.sendFile(__dirname + "/views/index.html");
 });
+
 
 // app.get("/json", (req,res)=>{
 //   res.json({
